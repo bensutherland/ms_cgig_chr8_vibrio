30fe97409f19873f4e8a29ebfedc155f9111c27c
bensutherland <bensutherland7@gmail.com>
2024-02-03 07:39:41 -0800

    Finalize well-to-sample annotation

--- 01_scripts/rhamp_04_well_to_sample.R
+++ 01_scripts/rhamp_04_well_to_sample.R
@@ -2,12 +2,31 @@
 # B. Sutherland, Liam Surry, VIU (initialized 2024-01-11)
 
 # Note: requires that 01_scripts/rhamp_03_call_genos.R has been run
-#        and remains in environment
+
+#### 00. Front Matter ####
+# Clear space
+# rm(list=ls())
+
+## Load libraries
+
+## Set working directory
+current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
+current.path <- gsub(pattern = "\\/01_scripts", replacement = "", x = current.path) # take main directory
+setwd(current.path)
+rm(current.path)
+
+# Load data
+load(file = "03_results/prepared_rhamp_results.RData")
 
 # Set user variables
-sample_info.FN <- ""
+sample_info.FN <- "~/Documents/00_sbio/VIU/VIU_oyster/CHR8/rhAmp_assay/sample_interp_from_LS_2024-01-21/OSU_CHR8_VC_Mapping_Family114-117_2024-01-21_MFrhAmpDNAid.txt"
+
+### Load sample info data
+annot.df <- read.delim(file = sample_info.FN, header = T, sep = "\t")
+head(annot.df)
 
 
+### Format full geno data
 head(all_plates.df)
 
 # Reduce the cols
@@ -16,6 +35,18 @@ data.df  <-    all_plates.df[,
 
 data.df <- as.data.frame(data.df)
 head(data.df)
+
+
+#### Connect the full.id in the data to the full.id in the annotation file
+data_annot.df <- merge(x = data.df, y = annot.df, by = "full.id", all.x = T)
+head(data_annot.df)
+
+
+write.table(x = data_annot.df, file = "03_results/rhAmp_full_results.txt")
+
+
+
+
 
 # # Separate full ID into components
 # data.df    <-  separate(data = data.df, col = "full.id"
