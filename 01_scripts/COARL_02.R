359407559605bc616e55561afd81a73745a00d0c
GreenT <bensutherland7@gmail.com>
2024-02-12 11:39:51 -0800

    Update proportion estimation of offspring pools

--- 01_scripts/COARL_02.R
+++ 01_scripts/COARL_02.R
@@ -22,14 +22,16 @@ head(cross_and_pheno.df)
 geno.df <- obj$tab
 geno.df[1:5,1:5]
 
-# Create loci dataframe
+### Infer offspring allele frequencies for each family in the cross
+# Create locus dataframe
 loci <- locNames(obj)
 loci.df <- as.data.frame(loci)
 rm(loci)
 head(loci.df)
 
+# Set nulls
+family <- NULL; dam <- NULL; column_of_interest <- NULL
 
-family <- NULL; dam <- NULL; loci.df <- NULL
 # full: nrow(cross_and_pheno.df)
 for(i in 1:2){
   
@@ -42,21 +44,49 @@ for(i in 1:2){
   # sire
   sire <- cross_and_pheno.df[i, "Sire"]
   
+  # Reporting
+  print(paste0("Family ", family, " is comprised of ", dam, " and ", sire))
+  
+  # Create an empty column for inferred numbers of alt alleles per locus
   loci.df <- cbind(loci.df, rep(NA, times = nrow(loci.df)))
   
-  colnames(loci.df)[i+1] <- paste0("family_", family, "_a2_count")
-  head(loci.df)
+  # Set the column name for the family
+  column_of_interest <- paste0("family_", family, "_a2_ppn")
+  colnames(loci.df)[i+1] <- column_of_interest
   
+  # Reporting
+  print("Starting to fill estimated proportions")
+  print(head(loci.df))
+  
+  # Set nulls
   num.alt <- NULL; mname <- NULL
+  
   for(m in 1:nrow(loci.df)){
     
+    # Marker name for this iteration
     mname <- paste0(loci.df[m, "loci"], ".1")
     
-    # pull the marker number of alt alleles for dam and sire combined
-    loci.df[m,i+1] <- geno.df[dam, mname] + 
-                           geno.df[sire, mname]
+    # Extract the number of alt alleles for each parent (order does not matter)
+    loci.df[m, column_of_interest] <- paste0(sort(c(geno.df[dam, mname], geno.df[sire, mname])), collapse = "__")
     
   }
+  
+  ## Fix the proportion data
+  # if any NA, cannot estimate
+  loci.df[, column_of_interest] <- gsub(pattern = "NA", replacement = "NA", x = loci.df[, column_of_interest])
+  
+  # One parent has one alt allele 
+  loci.df[, column_of_interest] <- gsub(pattern = "0__1", replacement = "0.5", x = loci.df[, column_of_interest])
+  
+  # Both parents have one alt allele
+  loci.df[, column_of_interest] <- gsub(pattern = "1__1", replacement = "1", x = loci.df[, column_of_interest])
+  
+  # One parent has one alt allele, other parent has two alt alleles
+  loci.df[, column_of_interest] <- gsub(pattern = "1__2", replacement = "1.5", x = loci.df[, column_of_interest])
+  
+  # Both parents have two alt alleles
+  loci.df[, column_of_interest] <- gsub(pattern = "1__2", replacement = "1.5", x = loci.df[, column_of_interest])
+  
 }
 
 
