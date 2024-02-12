c1ea3961267a072f78d65ebe9db783e8a1d327bc
GreenT <bensutherland7@gmail.com>
2024-02-12 09:47:39 -0800

    Uses new annotation file (csv)

--- 01_scripts/rhamp_04_well_to_sample.R
+++ 01_scripts/rhamp_04_well_to_sample.R
@@ -7,8 +7,6 @@
 # Clear space
 # rm(list=ls())
 
-## Load libraries
-
 ## Set working directory
 current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
 current.path <- gsub(pattern = "\\/01_scripts", replacement = "", x = current.path) # take main directory
@@ -18,13 +16,14 @@ rm(current.path)
 # Load data
 load(file = "03_results/prepared_rhamp_results.RData")
 
-# Set user variables
-sample_info.FN <- "00_archive/OSU_CHR8_VC_Mapping_Family114-117_2024-01-21_MFrhAmpDNAid.txt"
-
 ### Load sample info data
-annot.df <- read.delim(file = sample_info.FN, header = T, sep = "\t")
+annot.df <- read.delim(file = sample_info.FN, header = T, sep = ",")
 head(annot.df)
 
+# Adjust sample info
+annot.df <- annot.df[,1:3]
+colnames(annot.df)[1] <- "full.id"
+head(annot.df)
 
 ### Format full geno data
 head(all_plates.df)
@@ -40,20 +39,10 @@ head(data.df)
 #### Connect the full.id in the data to the full.id in the annotation file
 data_annot.df <- merge(x = data.df, y = annot.df, by = "full.id", all.x = T)
 head(data_annot.df)
-
+dim(data_annot.df)
 
 write.table(x = data_annot.df, file = "03_results/rhAmp_full_results.txt", quote = F, sep = "\t", row.names = F)
 
 
 
-# Next: go to 01_scripts/rhamp_04_well_to_sample.R
-
-# # Separate full ID into components
-# data.df    <-  separate(data = data.df, col = "full.id"
-#                      , into = c("plate.id", "well.id")
-#                      , sep = "__", remove = T)
-# 
-# head(data.df)
-
-
-
+# Next: go to 01_scripts/rhamp_05_eval_tech_reps.R
