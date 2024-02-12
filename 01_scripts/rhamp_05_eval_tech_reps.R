71e9fac793ab01ac7ee4fc92b7daebc38869dbd2
GreenT <bensutherland7@gmail.com>
2024-02-12 12:31:45 -0800

    Summarize per-sample genotypes based on tech reps

--- 01_scripts/rhamp_05_eval_tech_reps.R
+++ 01_scripts/rhamp_05_eval_tech_reps.R
@@ -10,4 +10,65 @@ head(data_annot.df)
 data_annot.df <- data_annot.df[order(data_annot.df$DNA.id), ]
 head(data_annot.df)
 head(data_annot.df, n = 100)
-View(data_annot.df)
+#View(data_annot.df)
+
+samples <- unique(data_annot.df$DNA.id)
+length(samples)
+
+# Remove no genos
+dim(data_annot.df)
+data_annot.df.bck <- data_annot.df
+
+# Remove rows without a geno call
+data_annot.df <- data_annot.df[data_annot.df$geno!="no.geno", ]
+dim(data_annot.df)
+
+# Make a new dataframe
+results.df <- as.data.frame(samples)
+head(results.df)
+rm(samples)
+
+# Make an empty column to fill with the most frequently viewed genotype
+results.df$majority.geno <- rep(NA, times = nrow(results.df))
+
+# Make an empty column to fill with the percentage of calls as the most frequent geno
+results.df$ppn.corr <- rep(NA, times = nrow(results.df))
+
+# Make an empty column to fill with the number of total calls (with a genotype)
+results.df$num.calls <- rep(NA, times = nrow(results.df))
+
+head(results.df)
+
+# Loop over samples
+sample <- NULL; slice <- NULL; majority.geno <- NULL
+for(i in 1:nrow(results.df)){
+  
+  # Sample of interest
+  sample <- results.df[i,"samples"]
+  
+  # Reporting
+  #print(paste0("Working on sample ", sample))
+  
+  # Take the data for the target sample
+  slice <- data_annot.df[data_annot.df$DNA.id==sample, ]
+  
+  # Define the majority geno
+  majority.geno <- names(head(sort(table(slice$geno), decreasing = TRUE), n = 1))
+  
+  # Fill in the value for the majority geno
+  results.df[results.df$samples==sample, "majority.geno"] <- majority.geno
+
+  # Fill in the value for the proportion correct
+  results.df[results.df$samples==sample, "ppn.corr"]      <- round(x = nrow(slice[slice$geno==majority.geno, ]) / nrow(slice), digits = 3)
+  
+  # Fill in the value for the number calls
+  results.df[results.df$samples==sample, "num.calls"]   <- nrow(slice)
+
+  
+}
+
+
+results.df
+
+write.table(x = results.df, file = "03_results/rhAmp_per_sample_genotype_summary.txt", quote = F, sep = "\t", row.names = F)
+
