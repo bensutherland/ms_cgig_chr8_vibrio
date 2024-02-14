edf026f856534befd118b80ec26dec9a0be7426b
bensutherland <bensutherland7@gmail.com>
2024-02-13 19:49:19 -0800

    Initialize GWAS prep script

---
+++ 01_scripts/COARL_03.R
@@ -0,0 +1,15 @@
+# Prepare data for GWAS
+# B. Sutherland (2024-02-14)
+
+# Requires that 01_scripts/COARL_02.R was already run
+# Clear workspace, launch simple_pop_stats
+
+load("03_results/per_family_inferred_allele_frequency_data.RData")
+
+#### 05. Prepare for GWAS ####
+
+
+
+
+
+# Next: gemma analysis (see README)
