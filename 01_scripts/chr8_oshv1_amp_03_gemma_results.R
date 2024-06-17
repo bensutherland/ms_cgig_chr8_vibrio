61a6686bc9f38923556c138fa9b85b8ec038006e
GreenT <bensutherland7@gmail.com>
2024-06-17 15:18:49 -0700

    Add plotting script for gemma results

---
+++ 01_scripts/chr8_oshv1_amp_03_gemma_results.R
@@ -0,0 +1,60 @@
+# Analyze gemma files
+#  requires (...)
+#  initialized 2024-06-17
+#  Ben J. G. Sutherland (VIU), incl. code dev by Konstantin Divilov
+
+#### 00. Front Matter ####
+# Clear space
+# rm(list=ls())
+
+# Load libraries
+library(vcfR)
+#devtools::install_github('kaustubhad/fastman',build_vignettes = TRUE)
+library(fastman)
+#install.packages("missMethods")
+library(missMethods)
+library(tidyr)
+library(ggplot2)
+
+## Set working directory
+current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
+current.path <- gsub(pattern = "\\/01_scripts", replacement = "", x = current.path) # take main directory
+setwd(current.path)
+rm(current.path)
+
+# User set variables
+gemma_output.FN       <- "03_results/output/gwas_all_fam_covar.assoc.txt"
+
+gemma_gwas <- read.table(file = gemma_output.FN, header = T)
+head(gemma_gwas)
+
+gemma_gwas <- as.data.frame(gemma_gwas)
+gemma_gwas <- separate(data = gemma_gwas, col = "rs", into = c("chr.true", "pos.true"), sep = "__", remove = F)
+head(gemma_gwas)
+gemma_gwas$pos.true <- as.numeric(gemma_gwas$pos.true)
+
+# convert to linkage group (LG), based on https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_902806645.1/
+# LG to Chr info can be found in Sup File in https://doi.org/10.1093/gigascience/giab020
+gemma_gwas$chr <- gemma_gwas$chr.true
+gemma_gwas$chr <- gsub(pattern = "NC_047559.1", replacement = "Chr7", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047560.1", replacement = "Chr1", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047561.1", replacement = "Chr9", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047562.1", replacement = "Chr6", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047563.1", replacement = "Chr3", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047564.1", replacement = "Chr2", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047565.1", replacement = "Chr4", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047566.1", replacement = "Chr5", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047567.1", replacement = "Chr10", x = gemma_gwas$chr)
+gemma_gwas$chr <- gsub(pattern = "NC_047568.1", replacement = "Chr8", x = gemma_gwas$chr)
+
+gemma_gwas <- gemma_gwas[with(gemma_gwas, order(gemma_gwas$chr)), ]
+
+hist(gemma_gwas$p_wald, breaks = 20)
+
+par(mfrow = c(1,1), mar = c(5,4,4,2) +0.1, mgp = c(3,1,0))
+fastman(m = gemma_gwas, chr = "chr", bp = "pos.true", p = "p_wald"
+        , genomewideline = -log10(0.05/nrow(gemma_gwas))
+        , suggestiveline = -log10(0.05)
+        , cex = 1, cex.lab = 1, cex.axis = 1
+        , ylim = c(0,6)
+        )
