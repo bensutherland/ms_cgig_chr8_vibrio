b036a6b1430307ad92fbb357dc0c36678022a24c
GreenT <bensutherland7@gmail.com>
2024-07-09 08:43:37 -0700

    Calc proportion and improve visualization

--- 01_scripts/comp_tech_parents.R
+++ 01_scripts/comp_tech_parents.R
@@ -18,8 +18,8 @@ setwd(current.path)
 rm(current.path)
 
 # Set variable names
-panel_records.FN <- "../compare_amp_panel_parents_and_wgrs_parents/0002.vcf"
-wgrs_records.FN  <- "../compare_amp_panel_parents_and_wgrs_parents/0003.vcf"
+panel_records.FN <- "../compare_amp_panel_parents_and_wgrs_parents/0002.vcf" # records from panel shared in both
+wgrs_records.FN  <- "../compare_amp_panel_parents_and_wgrs_parents/0003.vcf" # records from wgrs shared in both
 
 # Read in data
 panel.vcf <- read.vcfR(file = panel_records.FN)
@@ -28,15 +28,15 @@ wgrs.vcf  <- read.vcfR(file = wgrs_records.FN)
 panel.vcf
 wgrs.vcf
 
-#TODO: this is where we could limit to only hotspot loci
-
+# Extract genotype portion
 panel.df <- extract.gt(panel.vcf)
 wgrs.df  <- extract.gt(wgrs.vcf)
 
 panel.df[1:5,1:5]
 wgrs.df[1:5,1:5]
 
-# Update colnames to make match
+## Update indiv names to match the two datatypes
+# panel
 panel_indiv_names.df <- colnames(panel.df)
 panel_indiv_names.df <- as.data.frame(panel_indiv_names.df)
 colnames(panel_indiv_names.df) <- "indiv"
@@ -50,6 +50,7 @@ panel_indiv_names.df$indiv <- gsub(pattern = "OCP_171_1", replacement = "58-33M_
 panel_indiv_names.df$indiv <- gsub(pattern = "OCP_177_1", replacement = "58-9F_p", x = panel_indiv_names.df$indiv)
 colnames(panel.df) <- panel_indiv_names.df$indiv
 
+# wgrs
 wgrs_indiv_names.df <- colnames(wgrs.df)
 wgrs_indiv_names.df <- as.data.frame(wgrs_indiv_names.df)
 colnames(wgrs_indiv_names.df) <- "indiv"
@@ -57,86 +58,124 @@ wgrs_indiv_names.df <- separate(data = wgrs_indiv_names.df, col = "indiv", into 
 wgrs_indiv_names.df$full_name <- paste0(wgrs_indiv_names.df$fam, "-", wgrs_indiv_names.df$ind, "_g")
 colnames(wgrs.df) <- wgrs_indiv_names.df$full_name
 
-
+# Inspect
 panel.df[1:5,]
 wgrs.df[1:5,1:5]
 
-# Combine the two dfs
+# Combine the dfs based on shared rownames (loci)
 all.df <- cbind(panel.df, wgrs.df)
 dim(all.df)
 all.df[1:5,1:5]
+
+# Order so that same indivs w diff tech are beside each other
 all.df <- all.df[, order(colnames(all.df))]
 all.df[1:5,]
 
-
-# Drop non-hotspot loci
+# Remove non-hotspot loci (all hotspot loci have mnames that consist of a single numeric string)
 all_hotspot.df <- all.df[grep(pattern = "_", x = rownames(all.df), invert = T), ]
 all_hotspot.df[1:10,1:8]
 
-
+# Rename df
 dat.df <- all_hotspot.df
 
-# Loop to build tally
-eval.df <- NULL
+## Loop to tally and calculate proportions correct
+# Create empty df
+eval.df <- NULL # output df name
 eval.df$mname <- rownames(dat.df)
-eval.df <- as.data.frame(eval.df)
-head(eval.df)
 eval.df$na.tally <- NA
 eval.df$tally <- NA
+eval.df$pair.tally <- NA
+eval.df <- as.data.frame(eval.df)
+head(eval.df)
 
-# unique parents
+# Define unique indivs
 unique_inds.vec <- unique(gsub(pattern = "_g|_p", replacement = "", x = colnames(dat.df)))
 
-moi <- NULL
+# Set nulls
+moi <- NULL; tally <- NULL; na.tally <- NULL; pair.tally <- NULL; 
+panel.call <- NULL; genome.call <- NULL
+
+# Loop
 for(i in 1:length(eval.df$mname)){
   
-  print(i)
+  ## Debugging
+  #print(i)
   
+  # Select the mname for this iteration
   moi <- eval.df$mname[i]
+  
+  # Reset tallies to zero for this mname
   tally <- 0
   na.tally <- 0
-  panel.call <- NULL
-  genome.call <- NULL
+  pair.tally <- 0
   
-  for(ind in 1:length(unique_inds.vec)){
+  # Evaluate across indivs
+  for(j in 1:length(unique_inds.vec)){
     
-    print(ind)
+    ## Debugging
+    #print(j)
     
-    panel.call  <- dat.df[moi, which(colnames(dat.df)==paste0(unique_inds.vec[ind], "_p"))]
-    genome.call <- dat.df[moi, paste0(unique_inds.vec[ind], "_g")]
+    # Determine call by panel and wgrs for this indiv and mname
+    panel.call  <- dat.df[moi, paste0(unique_inds.vec[j], "_p")]
+    genome.call <- dat.df[moi, paste0(unique_inds.vec[j], "_g")]
     
+    # If neither call is NA
     if(!is.na(panel.call) & !is.na(genome.call)){
       
+      # Record that a pair is evaluable
+      pair.tally <- pair.tally + 1
+      
+      # If the two calls are concordant
       if(panel.call==genome.call){
         
+        # Record that a concordant call was made
         tally <- tally + 1
         
       }else{
         
+        # Keep concordant tally constant (no addition)
         tally <- tally
         
       }
-      
+    
+    # If an NA is observed  
     }else{
       
+      # Record the number of NAs observed
       na.tally <- na.tally + sum(is.na(c(panel.call, genome.call)))
       
     }
      
   }
   
-  eval.df[i,"na.tally"] <- na.tally
-  eval.df[i,"tally"]    <- tally
+  # Record the tallies for the mname
+  eval.df[i,"tally"]         <- tally
+  eval.df[i,"na.tally"]      <- na.tally
+  eval.df[i,"pair.tally"]    <- pair.tally
     
 }
   
+
 head(eval.df)
 
-pdf(file = "03_results/histogram_of_concordance_tally.pdf", width = 6.5, height = 4.5)
-hist(eval.df$tally)
+# Add a percentage correct (i.e., how many of the present pairs were found to be concordant?)
+eval.df$percent.corr <- eval.df$tally / eval.df$pair.tally
+head(eval.df)
+
+pdf(file = "03_results/histogram_concordant_calls.pdf", width = 7.5, height = 4.75)
+par(mfrow=c(1,2))
+hist(eval.df$percent.corr
+     , xlab = "Proportion concordant (no NAs) per marker", las = 1
+     , main = ""
+)
+hist(eval.df$tally
+     , xlab = "Number concordant calls per marker", las = 1
+     , main = ""
+     )
 dev.off()
 
 
 write.table(x = eval.df, file = "03_results/concordance_eval.txt", sep = "\t", row.names = T, col.names = NA)
-write.table(x = dat.df, file = "03_results/concordance_input_data.txt", sep = "\t", row.names = T, col.names = NA)
+write.table(x = dat.df, file = "03_results/concordance_calls.txt", sep = "\t", row.names = T, col.names = NA)
 
+# end
