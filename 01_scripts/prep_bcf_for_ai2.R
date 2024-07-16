5fbe6fb8eadee306527504a2858d93cdc6089995
GreenT <bensutherland7@gmail.com>
2024-07-16 09:43:14 -0700

    Screen out triallelic SNPs that made it through filters, and improve write-out speed

--- 01_scripts/prep_bcf_for_ai2.R
+++ 01_scripts/prep_bcf_for_ai2.R
@@ -8,9 +8,11 @@
 ## Install and load packages
 #install.packages("rstudioapi")
 #install.packages("vcfR")
+#install.packages("data.table")
 library("rstudioapi")
 library("vcfR")
 library("adegenet")
+library("data.table")
 
 # Set working directory to the ms_scallop_popgen repo
 current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
@@ -22,7 +24,8 @@ rm(current.path)
 # sessionInfo()
 
 # Set variables
-input.FN <- "04_impute_panel/wgrs_filtered_parent_loci_amp_panel_parent_loci_amp_panel_offspring_loci_NC_047559.1.vcf"
+#input.FN <- "04_impute_panel/wgrs_filtered_parent_loci_amp_panel_parent_loci_amp_panel_offspring_loci_NC_047559.1.vcf"
+input.FN <- "04_impute_panel/wgrs_filtered_parent_loci_amp_panel_parent_loci_amp_panel_offspring_loci_NC_047567.1.vcf"
 
 #### 01. Import data ####
 input.vcf <- vcfR::read.vcfR(file = input.FN)
@@ -42,35 +45,51 @@ genos.df[1:5,1:5]
 # genos.df[1:5,1:20]
 # str(genos.df)
 
-# Convert all NA to "9
+# Convert all missing data (NA) to "9
 genos.df[is.na(genos.df)] <- "9"
+genos.df[1:10,1:10]
 
 # Convert to alt allele dosage
 genos.df <- gsub(pattern = "0/0", replacement = "0", x = genos.df)
 genos.df <- gsub(pattern = "0/1", replacement = "1", x = genos.df)
 genos.df <- gsub(pattern = "1/1", replacement = "2", x = genos.df)
 
+# Temporary fix, drop triallelic site
+genos.df <- gsub(pattern = "1/2", replacement = "9", x = genos.df)
+genos.df <- gsub(pattern = "0/2", replacement = "9", x = genos.df)
+genos.df <- gsub(pattern = "2/2", replacement = "9", x = genos.df)
+
 # Convert to df
 genos.df <- as.data.frame(genos.df)
-genos.df[1:5,1:10]
+genos.df[1:10, 1:10]
 
 # Transpose
 genos_t.df <- t(genos.df)
-genos_t.df[1:5,1:5]
 genos_t.df <- as.data.frame(genos_t.df)
-genos_t.df[1:5,1:5]
+genos_t.df[1:10,1:4]
+
+# Make sure all loci for an individual are what are expected
 #unique(genos_t.df[which(rownames(genos_t.df)=="55-41F"),])
 
+# Create a vector of individual names
 indiv_names.df <- rownames(genos_t.df)
 indiv_names.df <- as.data.frame(indiv_names.df)
+colnames(indiv_names.df) <- "ind"
+head(indiv_names.df)
 
+# Combine ind names and genotypes
 genos_t.df <- cbind(indiv_names.df, genos_t.df)
-genos_t.df[1:5,1:5]
+genos_t.df[1:10,1:4]
 
-write.table(x = genos_t.df, file = "04_impute_panel/genos.txt", sep = " ", quote = F
-            , row.names = F, col.names = F
-            )
+# # Write output
+# write.table(x = genos_t.df, file = "04_impute_panel/genos.txt"
+#             , sep = " "
+#             , quote = F
+#             , row.names = F, col.names = F
+#             )
 
+# Write output
+data.table::fwrite(x = genos_t.df, file = "04_impute_panel/genos_fwrite.txt", quote = F, sep = " ", row.names = F, col.names = F)
 
 
 #### 03. Prepare pedigree file ####
@@ -84,4 +103,5 @@ head(pedigree.df)
 write.table(x = pedigree.df, file = "04_impute_panel/pedigree.txt", sep = " ", quote = F
             , row.names = F, col.names = F
 )
+
 
