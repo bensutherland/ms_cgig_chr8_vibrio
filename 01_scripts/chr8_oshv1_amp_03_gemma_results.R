7f4dddb8203f1883a22a9a93427d75d40fbd40e2
GreenT <bensutherland7@gmail.com>
2024-10-22 13:41:45 -0700

    Minor updates to amp to GEMMA

--- 01_scripts/chr8_oshv1_amp_03_gemma_results.R
+++ 01_scripts/chr8_oshv1_amp_03_gemma_results.R
@@ -23,6 +23,7 @@ setwd(current.path)
 rm(current.path)
 
 # User set variables
+<<<<<<< Updated upstream
 input_folder <- "03_results/output_pheno_survival_state"
 #input_folder <- "03_results/output_pheno_DPE"
 input_folder <- "03_results_denovo_genos/output_pheno_survival_state_GR70_noimpute" # denovo variants
@@ -30,6 +31,14 @@ input_folder <- "03_results_denovo_genos/output_pheno_survival_state_GR70_noimpu
 # Build output filename
 gemma_output.FN  <- paste0(input_folder, "/gwas_all_fam_covar.assoc.txt")
 
+=======
+#gemma_output.FN       <- "03_results/output_pheno_survival_state/gwas_all_fam_covar.assoc.txt"
+# gemma_output.FN       <- "03_results/output_pheno_DPE/gwas_all_fam_covar.assoc.txt"
+#gemma_output.FN <- "03_results/family_sep_gemma_inputs_2024-10-11/F114/output/result.assoc.txt"
+#gemma_output.FN <- "03_results/family_sep_gemma_inputs_2024-10-11/F115/output/result.assoc.txt"
+#gemma_output.FN <- "03_results/family_sep_gemma_inputs_2024-10-11/F116/output/result.assoc.txt"
+gemma_output.FN <- "03_results/family_sep_gemma_inputs_2024-10-11/F117/output/result.assoc.txt"
+>>>>>>> Stashed changes
 
 #### 01. Load GEMMA results ####
 # Read in GEMMA output
@@ -61,6 +70,8 @@ gemma_gwas <- gemma_gwas[with(gemma_gwas, order(gemma_gwas$chr)), ]
 # Output plot filename
 plot.FN <- gsub(pattern = "03_results\\/", replacement = "", x = gemma_output.FN)
 plot.FN <- gsub(pattern = "\\/gwas_all_fam_covar.assoc.txt", replacement = "", x = plot.FN)
+plot.FN <- gsub(pattern = "\\/result.assoc.txt", replacement = "", x = plot.FN)
+plot.FN <- gsub(pattern = "\\/", replacement = "_", x = plot.FN)
 
 # Plot histogram of pvals
 pdf(file = paste0(plot.FN, "/pval_hist.pdf"), width = 7, height = 4.5)
@@ -70,10 +81,15 @@ dev.off()
 # Create directory for output (if does not already exist)
 dir.create(path = "03_results/GWAS_plots")
 
+<<<<<<< Updated upstream
 # Manhattan plot
 output_folder <- gsub(pattern = "\\/.*", replacement = "", x = input_folder)
 name_details <- gsub(pattern = ".*\\/", replacement = "", x = input_folder)
 pdf(file = paste0(output_folder, "/GWAS_plots/Manhattan_", name_details, ".pdf"), width = 9, height = 3.5)
+=======
+
+pdf(file = paste0("03_results/GWAS_plots/Manhattan_", plot.FN, ".pdf"), width = 9, height = 5)
+>>>>>>> Stashed changes
 par(mfrow = c(1,1), mar = c(5,4,4,2) +0.1, mgp = c(3,1,0))
 fastman(m = gemma_gwas, chr = "chr", bp = "pos.true", p = "p_wald"
         , genomewideline = -log10(0.05/nrow(gemma_gwas))
