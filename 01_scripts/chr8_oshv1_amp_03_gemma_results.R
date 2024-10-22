d93f5e3eb4c0617de681d434586f637b67b3f86c
GreenT <bensutherland7@gmail.com>
2024-10-22 13:35:47 -0700

    Minor adjustment

--- 01_scripts/chr8_oshv1_amp_03_gemma_results.R
+++ 01_scripts/chr8_oshv1_amp_03_gemma_results.R
@@ -23,8 +23,13 @@ setwd(current.path)
 rm(current.path)
 
 # User set variables
-#gemma_output.FN       <- "03_results/output_pheno_survival_state/gwas_all_fam_covar.assoc.txt"
-gemma_output.FN       <- "03_results/output_pheno_DPE/gwas_all_fam_covar.assoc.txt"
+input_folder <- "03_results/output_pheno_survival_state"
+#input_folder <- "03_results/output_pheno_DPE"
+input_folder <- "03_results_denovo_genos/output_pheno_survival_state_GR70_noimpute" # denovo variants
+
+# Build output filename
+gemma_output.FN  <- paste0(input_folder, "/gwas_all_fam_covar.assoc.txt")
+
 
 #### 01. Load GEMMA results ####
 # Read in GEMMA output
@@ -52,16 +57,23 @@ gemma_gwas$chr <- gsub(pattern = "NC_047568.1", replacement = "Chr08", x = gemma
 
 gemma_gwas <- gemma_gwas[with(gemma_gwas, order(gemma_gwas$chr)), ]
 
-hist(gemma_gwas$p_wald, breaks = 20)
-
 
 # Output plot filename
 plot.FN <- gsub(pattern = "03_results\\/", replacement = "", x = gemma_output.FN)
 plot.FN <- gsub(pattern = "\\/gwas_all_fam_covar.assoc.txt", replacement = "", x = plot.FN)
 
+# Plot histogram of pvals
+pdf(file = paste0(plot.FN, "/pval_hist.pdf"), width = 7, height = 4.5)
+hist(gemma_gwas$p_wald, breaks = 20)
+dev.off()
+
+# Create directory for output (if does not already exist)
 dir.create(path = "03_results/GWAS_plots")
 
-pdf(file = paste0("03_results/GWAS_plots/Manhattan_", plot.FN, ".pdf"), width = 9, height = 5)
+# Manhattan plot
+output_folder <- gsub(pattern = "\\/.*", replacement = "", x = input_folder)
+name_details <- gsub(pattern = ".*\\/", replacement = "", x = input_folder)
+pdf(file = paste0(output_folder, "/GWAS_plots/Manhattan_", name_details, ".pdf"), width = 9, height = 3.5)
 par(mfrow = c(1,1), mar = c(5,4,4,2) +0.1, mgp = c(3,1,0))
 fastman(m = gemma_gwas, chr = "chr", bp = "pos.true", p = "p_wald"
         , genomewideline = -log10(0.05/nrow(gemma_gwas))
@@ -71,5 +83,9 @@ fastman(m = gemma_gwas, chr = "chr", bp = "pos.true", p = "p_wald"
         )
 dev.off()
 
-# end
 
+##### End matter ####
+save.image(file = paste0(input_folder, "/", "post_Manhattan_plotting.RData"))
+
+
+# end
