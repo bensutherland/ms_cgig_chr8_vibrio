56f2509ead5b51c278c1c652d417fcf4fbe38622
GreenT <bensutherland7@gmail.com>
2024-11-21 13:50:37 -0800

    Keep only relevant scripts

--- 01_scripts/chr8_oshv1_amp_mhap_01.R
+++
@@ -1,5 +0,0 @@
-# Microhaplotype analysis
-# source amplitools initiator
-
-select_best_rep_fastq(input_folder = "12_input_mhap/", metadata.FN = "00_archive/filename_to_sample_map.txt"
-                      , counts.FN = "12_input_mhap/reads_per_sample_table.txt")
